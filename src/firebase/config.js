d1f3ced5e6fa99c4e853a630ff47435f705185f9
abdo-elmorsi <abdelrahmandiv@gmail.com>
2025-01-13 18:49:43 +0200

    env added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAI1YxMPZvZOG43QyzOKJj_6gyshsZv-mg",
-	authDomain: "images-store-602a5.firebaseapp.com",
-	projectId: "images-store-602a5",
-	// databaseURL: 'gs://images-store-602a5.appspot.com',
-	storageBucket: "images-store-602a5.appspot.com",
-	messagingSenderId: "1096750066156",
-	appId: "1:1096750066156:web:ad0800984582fb59cbf8c9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
